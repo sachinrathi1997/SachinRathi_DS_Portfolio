c910691cd089441021101becf57b11bb6b454050
Sachin Rathi <53687222+sachinrathi1997@users.noreply.github.com>
2024-08-22 16:16:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function addRecommendation() {
   if (recommendation.value != null && recommendation.value.trim() != "") {
     console.log("New recommendation added");
     //Call showPopup here
+    showPopup(true);
 
     // Create a new 'recommendation' element and set it's value to the user's message
     var element = document.createElement("div");
@@ -24,4 +25,5 @@ function showPopup(bool) {
   } else {
     document.getElementById('popup').style.visibility = 'hidden'
   }
+  
 }
